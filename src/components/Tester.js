4e2aebb9e39670faf27106b03277667a0b56196d
JAGormley <andrewjgormley@gmail.com>
2016-07-24 23:34:02 -0700

    init

---
+++ src/components/Tester.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Tester extends Component {
+  render() {
+    return (
+      <div className="Tester">
+        hello there
+      </div>
+    );
+  }
+}
+
+export default Tester;
