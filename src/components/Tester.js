eb5bb8b37e1231c7b10d956cc3b11b91661424f6
JAGormley <andrewjgormley@gmail.com>
2016-08-19 17:08:28 -0300

    Ajax calls working

--- src/components/Tester.js
+++ src/components/Tester.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 
 class Tester extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {chips: "ieieieieie"};
+        this.getTheThing = this.getTheThing.bind(this);
+    }
+
+
   render() {
     return (
       <div className="Tester">
-        hello there
+        <button onClick={this.getTheThing}>{this.state.chips}</button>
       </div>
     );
-  }
+  };
+
+  getTheThing() {
+      // url (required), options (optional)
+      fetch('http://localhost:9000/chips', {method: 'get'}).then(function(response) {
+        return response.json();
+      }).then(function(data) {
+       this.setState({chips: JSON.stringify(data.name, null, 4)});
+     }.bind(this));
+
+  };
 }
 
 export default Tester;
